487e7ee2f49c18322703314ed57e4aa80b201e71
Tal <talrofeh@gmail.com>
2021-07-13 16:59:25 +0300

    fix naming and IUSERMOD

--- PrisonHelicopter/PrisonHelicopterMod.cs
+++ PrisonHelicopter/PrisonHelicopterMod.cs
@@ -4,7 +4,7 @@ using CitiesHarmony.API;
 using ICities;
 
 namespace PrisonHelicopter {
-    public class PrisonHelicopterMod : LoadingExtensionBase, IUserMod {
+    public class PrisonHelicopterMod : IUserMod {
 
         string IUserMod.Name => "Prison Helicopter Mod";
         string IUserMod.Description => "Allow the police helicopter depot to spawn prison helicopters to transport prisoners to jail";
