db84b8e5e0d6277ba615ff64205a5230402439b8
Tal <talrofeh@gmail.com>
2021-08-16 00:38:14 +0300

    fixes to new transfer reason and old asset issues

---
+++ PrisonHelicopter/HarmonyPatches/OldAssetsCompatibilityPatch.cs
@@ -0,0 +1,24 @@
+using HarmonyLib;
+
+
+namespace PrisonHelicopter.HarmonyPatches
+{
+    [HarmonyPatch(typeof(PackageHelper), "ResolveLegacyTypeHandler")]
+    static class OldAssetsCompatibilityPatch
+    {
+        // 'PrisonHelicopter.AI.NewPoliceStationAI.NewPoliceStationAI, PrisonHelicopter, Version=1.0.7890.49, Culture=neutral, PublicKeyToken=null'
+        [HarmonyPostfix]
+        public static void Postfix(ref string __result)
+	    {
+            string[] temp = __result.Split(',');
+            if(temp[1] == " PrisonHelicopter")
+            {
+                if(temp[0] == "PrisonHelicopter.AI.NewPoliceStationAI.NewPoliceStationAI")
+                {
+                    __result = "PrisonHelicopter.AI.NewPoliceStationAI, PrisonHelicopter, Version=1.0.7890.49, Culture=neutral, PublicKeyToken=null";
+                }
+            }
+		    LogHelper.Information(__result);
+	    }
+    }
+}
