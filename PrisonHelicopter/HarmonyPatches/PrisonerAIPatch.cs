3763630d8fff3283e0f7830d0382dbc858a770cb
Tal <talrofeh@gmail.com>
2021-08-17 01:14:21 +0300

    prsioners now show "serving time" instead of visiting

--- PrisonHelicopter/HarmonyPatches/PrisonerAIPatch.cs
+++ PrisonHelicopter/HarmonyPatches/PrisonerAIPatch.cs
@@ -1,16 +1,18 @@
 using ColossalFramework;
 using HarmonyLib;
 using System;
-using UnityEngine;
+using System.Reflection;
 
 namespace PrisonHelicopter.PrisonHelicopter.HarmonyPatches {
 
     [HarmonyPatch(typeof(PrisonerAI))]
     public static class PrisonerAIPatch {
 
-        [HarmonyPatch(typeof(PrisonerAI), "GetLocalizedStatus")]
+        [HarmonyPatch(typeof(PrisonerAI), "GetLocalizedStatus",
+            new Type[] { typeof(ushort), typeof(CitizenInstance), typeof(InstanceID) },
+            new ArgumentType[] { ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Out })]
         [HarmonyPrefix]
-        public static bool GetLocalizedStatus(PrisonerAI __instance, ushort instanceID, ref CitizenInstance data, out InstanceID target, ref string __result)
+        public static bool Prefix1(PrisonerAI __instance, ushort instanceID, ref CitizenInstance data, out InstanceID target, ref string __result)
 	{
 	    if ((data.m_flags & (CitizenInstance.Flags.Blown | CitizenInstance.Flags.Floating)) != 0)
 	    {
@@ -32,7 +34,32 @@ namespace PrisonHelicopter.PrisonHelicopter.HarmonyPatches {
 	            __result = ColossalFramework.Globalization.Locale.Get("CITIZEN_STATUS_CONFUSED");
                 }
             }
-            target = InstanceID.Empty;
+	    return false;
+	}
+
+        [HarmonyPatch(typeof(PrisonerAI), "GetLocalizedStatus",
+            new Type[] { typeof(uint), typeof(Citizen), typeof(InstanceID) },
+            new ArgumentType[] { ArgumentType.Normal, ArgumentType.Ref, ArgumentType.Out })]
+        [HarmonyPrefix]
+        public static bool Prefix2(PrisonerAI __instance, uint citizenID, ref Citizen data, out InstanceID target, ref string __result)
+	{
+	    CitizenManager instance = Singleton<CitizenManager>.instance;
+	    ushort instance2 = data.m_instance;
+	    if (instance2 != 0)
+	    {
+                Prefix1(__instance, instance2, ref instance.m_instances.m_buffer[instance2], out target, ref __result);
+	    }
+	    else if (data.m_visitBuilding != 0)
+	    {
+		target = InstanceID.Empty;
+		target.Building = data.m_visitBuilding;
+		__result = "Serving time at ";
+	    }
+            else
+            {
+                target = InstanceID.Empty;
+                __result = ColossalFramework.Globalization.Locale.Get("CITIZEN_STATUS_CONFUSED");
+            }
 	    return false;
 	}
     }
