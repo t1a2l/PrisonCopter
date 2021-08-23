8918ab4d9314c5454cd51c8f1b2245a2bc1518b5
Tal <talrofeh@gmail.com>
2021-08-24 01:48:36 +0300

    support intercity train and buses checkbox

--- PrisonHelicopter/HarmonyPatches/CityServiceWorldInfoPanelPatch.cs
+++ PrisonHelicopter/HarmonyPatches/CityServiceWorldInfoPanelPatch.cs
@@ -11,48 +11,38 @@ namespace PrisonHelicopter.HarmonyPatches
 
         private static string _originalTooltip;
 
-        private static string _originalCheckBoxText;
-
-        private static string _originalCheckBoxTooltip;
-
         [HarmonyPatch(typeof(CityServiceWorldInfoPanel), "UpdateBindings")]
         [HarmonyPostfix]
         internal static void Postfix1(CityServiceWorldInfoPanel __instance, InstanceID ___m_InstanceID, UIPanel ___m_intercityTrainsPanel)
         {
             var label = ___m_intercityTrainsPanel.Find<UILabel>("Label");
-            var checkbox = ___m_intercityTrainsPanel.Find<UICheckBox>("AcceptIntercityTrains");
             _originalLabel ??= label.text;
             _originalTooltip ??= label.tooltip;
-            _originalCheckBoxText ??= checkbox.text;
-            _originalCheckBoxTooltip ??= checkbox.tooltip;
             var building1 = ___m_InstanceID.Building;
             var instance = BuildingManager.instance;
             var building2 = instance.m_buildings.m_buffer[building1];
             var info = building2.Info;
             var buildingAi = info.m_buildingAI;
-            if(info.m_class.m_service == ItemClass.Service.PoliceDepartment && buildingAi is HelicopterDepotAI)
+            var newPoliceStationAI = buildingAi as NewPoliceStationAI;
+            var helicopterDepotAI = buildingAi as HelicopterDepotAI;
+            var policeHelicopterDepot = info.m_class.m_service == ItemClass.Service.PoliceDepartment && helicopterDepotAI;
+            var policeStation = info.m_class.m_service == ItemClass.Service.PoliceDepartment &&  info.m_class.m_level < ItemClass.Level.Level4 && newPoliceStationAI;
+            if(policeHelicopterDepot)
             {
-                ___m_intercityTrainsPanel.isVisible = true;
+                 ___m_intercityTrainsPanel.isVisible = true;
                 label.text = "Allow Prison Helicopters";
                 label.tooltip = "Disable this if you prefer to use this helicopter depot only for police helicopters";
-                checkbox.text = "Allow Prison Helicopters";
-                checkbox.tooltip = "Disable this if you prefer to use this helicopter depot only for police helicopters";
             }
-            else if(info.m_class.m_service == ItemClass.Service.PoliceDepartment &&  info.m_class.m_level < ItemClass.Level.Level4 && buildingAi is NewPoliceStationAI)
+            else if(policeStation)
             {
-                ___m_intercityTrainsPanel.isVisible = true;
+                 ___m_intercityTrainsPanel.isVisible = true;
                 label.text = "Allow Prison Helicopters and Police Vans fleet";
                 label.tooltip = "Disable this if you prefer to allow only police vans to pick up prisoners from large police stations and prison vans from prison";
-                checkbox.text = "Allow Prison Helicopters and Police Vans fleet";
-                checkbox.tooltip = "Disable this if you prefer to allow only police vans to pick up prisoners from large police stations and prison vans from prison";
-            }  
+            }
             else
             {
-                ___m_intercityTrainsPanel.isVisible = false;
                 label.text = _originalLabel;
-                label.tooltip = _originalTooltip;
-                checkbox.text = _originalCheckBoxText;
-                checkbox.tooltip = _originalCheckBoxTooltip;
+                label.tooltip =  _originalTooltip;
             }
         }
     }
