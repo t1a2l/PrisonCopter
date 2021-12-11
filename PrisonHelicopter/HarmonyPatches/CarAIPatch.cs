5bcb2ce64e4f1725a2d9a1b184f199f14a232ddd
Tal <talrofeh@gmail.com>
2021-12-11 21:24:27 +0200

    wip

--- PrisonHelicopter/HarmonyPatches/CarAIPatch.cs
+++ PrisonHelicopter/HarmonyPatches/CarAIPatch.cs
@@ -8,18 +8,26 @@ namespace PrisonHelicopter.HarmonyPatches {
     [HarmonyPatch(typeof(CarAI))]
     public static class CarAIPatch {
 
-        private delegate bool StartPathFindDelegate(ushort vehicleID, ref Vehicle vehicleData);
-        private static readonly StartPathFindDelegate StartPathFind = AccessTools.MethodDelegate<StartPathFindDelegate>(typeof(PoliceCarAI).GetMethod("StartPathFind", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(ushort), typeof(Vehicle).MakeByRefType() }, new ParameterModifier[0]), null, false);
+        private delegate bool StartPathFindPoliceCarAIDelegate(ushort vehicleID, ref Vehicle vehicleData);
+        private static readonly StartPathFindPoliceCarAIDelegate StartPathFindPoliceCarAI = AccessTools.MethodDelegate<StartPathFindPoliceCarAIDelegate>(typeof(PoliceCarAI).GetMethod("StartPathFind", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(ushort), typeof(Vehicle).MakeByRefType() }, new ParameterModifier[] { }), null, false);
 
         [HarmonyPatch(typeof(CarAI), "PathfindFailure")]
         [HarmonyPrefix]
         public static bool PathfindFailure(ushort vehicleID, ref Vehicle data)
 	{
             Building police_building = Singleton<BuildingManager>.instance.m_buildings.m_buffer[data.m_sourceBuilding];
-            if((police_building.Info.m_class.m_level >= ItemClass.Level.Level4 || police_building.Info.m_class.m_level < ItemClass.Level.Level4 && (police_building.m_flags & Building.Flags.Downgrading) == 0) && (data.m_flags & Vehicle.Flags.GoingBack) == 0)
+            Vehicle vehicle = Singleton<VehicleManager>.instance.m_vehicles.m_buffer[vehicleID];
+            var is_prison_van = false;
+            var is_prison = false;
+            var is_big_police_station = false;
+            if(vehicle.Info.GetAI() is PoliceCarAI &&  vehicle.Info.GetClassLevel() >= ItemClass.Level.Level4) is_prison_van = true;
+            if(police_building.Info.m_class.m_level >= ItemClass.Level.Level4) is_prison = true;
+            if(police_building.Info.m_class.m_level < ItemClass.Level.Level4 && (police_building.m_flags & Building.Flags.Downgrading) == 0) is_big_police_station = true;
+
+            if(is_prison_van && (data.m_flags & Vehicle.Flags.GoingBack) == 0 && (is_prison || is_big_police_station))
             {
                 data.m_flags |= Vehicle.Flags.GoingBack;
-                if(!StartPathFind(vehicleID, ref data))
+                if(!StartPathFindPoliceCarAI(vehicleID, ref data))
                 {
                     data.Unspawn(vehicleID);
                 }
