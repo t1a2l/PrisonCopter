9d337a92d7a2509c022ca2f524624c8a1bcb12f4
Tal <talrofeh@gmail.com>
2023-10-25 20:47:59 +0300

    get default helicopter count and police car count of police building

--- PrisonHelicopter/HarmonyPatches/InitializePrefabBuildingPatch.cs
+++ PrisonHelicopter/HarmonyPatches/InitializePrefabBuildingPatch.cs
@@ -17,16 +17,38 @@ namespace PrisonHelicopter.HarmonyPatches
                 if (__instance.m_class.m_service == ItemClass.Service.PoliceDepartment && __instance.m_class.m_subService != ItemClass.SubService.PoliceDepartmentBank && __instance.m_class.m_level != ItemClass.Level.Level3)
                 {
                     var oldAI = __instance.GetComponent<PrefabAI>();
-                    Object.DestroyImmediate(oldAI);
-                    var newAI = (PrefabAI)__instance.gameObject.AddComponent<PrisonCopterPoliceStationAI>();
-                    PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                    if (oldAI is PrisonCopterPoliceStationAI prisonCopter)
+                    {
+                        var count = prisonCopter.m_policeCarCount;
+                        Object.DestroyImmediate(oldAI);
+                        var newAI = (PrefabAI)__instance.gameObject.AddComponent<PrisonCopterPoliceStationAI>();
+
+                        PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                        if (newAI is PrisonCopterPoliceStationAI policeStationAI)
+                        {
+                            policeStationAI.m_policeCarCount = count;
+                            policeStationAI.m_policeVanCount = count / 2;
+                        }
+                    }
+
                 }
                 if (__instance.m_class.m_service == ItemClass.Service.PoliceDepartment && __instance.m_class.m_subService != ItemClass.SubService.PoliceDepartmentBank && __instance.m_class.m_level == ItemClass.Level.Level3)
                 {
                     var oldAI = __instance.GetComponent<PrefabAI>();
-                    Object.DestroyImmediate(oldAI);
-                    var newAI = (PrefabAI)__instance.gameObject.AddComponent<PoliceHelicopterDepotAI>();
-                    PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                    if(oldAI is HelicopterDepotAI helicopter)
+                    {
+                        var count = helicopter.m_helicopterCount;
+                        Object.DestroyImmediate(oldAI);
+                        var newAI = (PrefabAI)__instance.gameObject.AddComponent<PoliceHelicopterDepotAI>();
+
+                        PrefabUtil.TryCopyAttributes(oldAI, newAI, false);
+                        if(newAI is PoliceHelicopterDepotAI police)
+                        {
+                            police.m_policeHelicopterCount = count;
+                            police.m_prisonHelicopterCount = count;
+                        }
+                    }
+                    
                 }
             }
             catch (Exception e)
