1971c3c25db15a426ac152834c11be19fe2a17ae
Tal <talrofeh@gmail.com>
2026-04-30 23:15:19 +0300

    logging and select the correct offer

--- HarmonyPatches/ExtendedTransferManagerPatch.cs
+++ HarmonyPatches/ExtendedTransferManagerPatch.cs
@@ -1,31 +1,52 @@
 using System;
 using ColossalFramework;
 using HarmonyLib;
+using PrisonHelicopter.Utils;
 using PrisonHelicopter.Utils.TransfersBridge;
 using UnityEngine;
 
 namespace PrisonHelicopter.HarmonyPatches
 {
-    [HarmonyPatch]
     internal class ExtendedTransferManagerPatch
     {
-        public static void TryPatch(Harmony harmony)
+        private static bool patched = false;
+
+        public static void TryPatch()
         {
+            if (patched) return;
+
             if (TransfersAPI.Backend is not MoreTransferReasonsBackend)
+            {
+                LogHelper.Warning("MoreTransferReasonsBackend not found. ExtendedTransferManagerPatch will be unavailable.");
                 return;
+            }
 
             if (!TransfersAPI.Backend.IsAvailable)
+            {
+                LogHelper.Warning("TransfersAPI.Backend is not available. ExtendedTransferManagerPatch will be unavailable.");
                 return;
+            }
 
-            var prefix = AccessTools.Method(typeof(ExtendedTransferManagerPatch), nameof(ExtendedTransferManagerPatch.Prefix));
+            if (MoreTransferReasonsBridge._startTransfer == null)
+            {
+                LogHelper.Warning("MoreTransferReasonsBridge._startTransfer method not found. ExtendedTransferManagerPatch will be unavailable.");
+                return;
+            }
+
+            var prefix = AccessTools.Method(typeof(ExtendedTransferManagerPatch), nameof(Prefix));
+            var harmony = new Harmony(PatchUtil.HarmonyId);
             harmony.Patch(MoreTransferReasonsBridge._startTransfer, prefix: new HarmonyMethod(prefix));
+
+            patched = true;
+            LogHelper.Information("ExtendedTransferManagerPatch successfully applied.");
         }
 
         private static bool Prefix(object __instance, object material, object offerOut, object offerIn, int delta)
         {
             if (!TryHandle(material, offerOut, offerIn, delta))
+            {
                 return true;
-
+            }
             return false;
         }
 
@@ -34,7 +55,9 @@ namespace PrisonHelicopter.HarmonyPatches
             byte vanillaReason = Convert.ToByte(materialObj);
 
             if ((TransferManager.TransferReason)vanillaReason == TransferManager.TransferReason.None)
+            {
                 return false;
+            }
 
             var offerOut = ConvertOffer(offerOutObj);
             var offerIn = ConvertOffer(offerInObj);
@@ -92,17 +115,34 @@ namespace PrisonHelicopter.HarmonyPatches
         private static TransferManager.TransferOffer ConvertOffer(object boxedOffer)
         {
             var t = boxedOffer.GetType();
-            return new TransferManager.TransferOffer
+
+            var building = (ushort)(t.GetField("Building")?.GetValue(boxedOffer) ?? (ushort)0);
+            var vehicle = (ushort)(t.GetField("Vehicle")?.GetValue(boxedOffer) ?? (ushort)0);
+            var citizen = (uint)(t.GetField("Citizen")?.GetValue(boxedOffer) ?? 0u);
+
+            var transferOffer = new TransferManager.TransferOffer
             {
                 Active = (bool)t.GetField("Active").GetValue(boxedOffer),
                 Amount = (int)t.GetField("Amount").GetValue(boxedOffer),
-                Building = (ushort)t.GetField("Building").GetValue(boxedOffer),
-                Vehicle = (ushort)t.GetField("Vehicle").GetValue(boxedOffer),
-                Citizen = (uint)t.GetField("Citizen").GetValue(boxedOffer),
                 Position = (Vector3)t.GetField("Position").GetValue(boxedOffer),
                 m_isLocalPark = (byte)(t.GetField("m_isLocalPark")?.GetValue(boxedOffer) ?? (byte)0),
                 m_object = (InstanceID)(t.GetField("m_object")?.GetValue(boxedOffer) ?? default(InstanceID))
             };
+
+            if (building != 0)
+            {
+                transferOffer.Building = building;
+            }
+            else if(vehicle != 0)
+            {
+                transferOffer.Vehicle = vehicle;
+            }
+            else if (citizen != 0)
+            {
+                transferOffer.Citizen = citizen;
+            }
+
+            return transferOffer;
         }
     }
 }
